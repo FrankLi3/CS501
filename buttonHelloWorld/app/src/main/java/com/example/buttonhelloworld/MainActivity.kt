bec712dc56fb2a63bb1ea66e0cb0f2d9648c1d1c
Qinfeng Li <ql2016@bu.edu>
2024-09-09 09:18:50 -0400

    hw1

---
+++ buttonHelloWorld/app/src/main/java/com/example/buttonhelloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.buttonhelloworld
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun buttonOnClick(view: View?){
+        val textView = findViewById<TextView>(R.id.helloWorld)
+        textView.text = "Hello World!"
+    }
+}
